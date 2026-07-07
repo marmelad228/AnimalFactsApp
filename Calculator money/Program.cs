028a7f7b3e8c09cb5b3aa863b50361403c2e3751
Andrey Novoseltcev <n-andrei07@mail.ru>
2026-07-07 15:56:20 +0300

    Полное удаление мусора

---
+++ Calculator money/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_money
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float num;
+            float num1;
+            float res = 0;
+            Console.Write("Введите первое чсило: ");
+            num=Convert.ToSingle(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            num1=Convert.ToSingle(Console.ReadLine());
+            res = num + num1;
+            Console.Write($"Сумма ваших чисел: {res}");
+
+        }
+       
+
+
+    }
+
+}
